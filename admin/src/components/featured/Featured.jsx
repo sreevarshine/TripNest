9915adf5eafa9c5b47e91d7370e21a3983e9f661
sreevarshine <166473782+sreevarshine@users.noreply.github.com>
2024-11-13 02:33:00 +0530

    First Commit

--- admin/src/components/featured/Featured.jsx
+++ admin/src/components/featured/Featured.jsx
@@ -17,7 +17,7 @@ const Featured = () => {
           <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
         <p className="title">Total sales made today</p>
-        <p className="amount">$420</p>
+        <p className="amount">₹4200</p>
         <p className="desc">
           Previous transactions processing. Last payments may not be included.
         </p>
