2990e03428f50bfc90d816d2752167715196d39e
sreevarshine <166473782+sreevarshine@users.noreply.github.com>
2024-11-13 02:07:18 +0530

    First Commit

--- admin/src/components/widget/Widget.jsx
+++ admin/src/components/widget/Widget.jsx
@@ -83,7 +83,7 @@ const Widget = ({ type }) => {
       <div className="left">
         <span className="title">{data.title}</span>
         <span className="counter">
-          {data.isMoney && "$"} {amount}
+          {data.isMoney && "₹"} {amount}
         </span>
         <span className="link">{data.link}</span>
       </div>
